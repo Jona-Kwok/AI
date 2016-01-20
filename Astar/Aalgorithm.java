5eb32d4ae86857b323962f2724cbb77247fd7f4d
Jona-Kwok <kwokjona@gmail.com>
2016-01-20 13:22:44 +0200

    java#1
    the first v of Java

---
+++ Astar/Aalgorithm.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * 
+ */
+
+/**
+ * @author Jona
+ *
+ */
+public class Aalgorithm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] maze = {{0,0,0,1,0,0,0,0,0,0,0,0},
+				{0,0,0,1,0,0,1,1,1,1,1,1},
+				{1,0,0,1,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0,0,0,0},
+				{0,1,1,0,0,1,1,0,1,1,0,1},
+				{0,0,1,0,0,1,0,0,1,0,0,0},
+				{1,0,1,0,0,1,0,1,1,1,1,0},
+				{0,0,1,0,0,0,0,0,1,0,0,0},
+				{0,1,1,1,1,1,1,0,1,0,1,1},
+				{0,0,0,0,0,0,0,0,1,0,0,0},
+				{1,1,0,1,1,0,1,1,1,0,0,0},
+				{0,0,0,1,0,0,0,0,0,0,0,0}};
+		
+	}
+
+	public int[] findnext(int now[],int[][] map,int dest[])
+	{
+		int depth = map.length;
+		int width = map[0].length;
+		
+		return dest;
+	}
+}
---
+++ astar.m
@@ -0,0 +1,7 @@
+function path = astar(init,map,dest)
+
+end
+
+function next = findnext(now,map,dest)
+    
+end
