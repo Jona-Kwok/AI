286e98ada38a3d6efd9aa12f607557cedf5e03f8
Jona-Kwok <kwokjona@gmail.com>
2016-01-25 14:06:35 +0200

    25.Jan, basic astar

--- Astar/Aalgorithm.java
+++ Astar/Aalgorithm.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.IntPredicate;
 
 /**
  * @author Jona tree structure needed
  */
 public class Aalgorithm {
-	static int[][] maze = new int[][] { 
-			{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+	static int[][] maze = new int[][] { { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1 },
 			{ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -33,24 +33,41 @@ public class Aalgorithm {
 		int[] init = { 0, 0 };
 		int[] dest = { 11, 11 };
 
-		HashMap<Integer, String> queue = new HashMap<Integer, String>();
+		HashMap<String, Integer> queue = new HashMap<String, Integer>();
+		queue.put("[" + init[0] + "," + init[1] + "]" + "\n", 0);
+		myAstar(queue, dest);
 
-		astar(init, queue, dest, 0);
+		// HashMap<Integer, String> queue = new HashMap<Integer, String>();
+		//
+		// astar(init, queue, dest, 0);
 	}
 
-	public static void astar(int[] now, HashMap<Integer, String> queue,
-			int[] dest, int step) throws IOException {
+	public static void myAstar(HashMap<String, Integer> queue, int[] dest) {
+		int min = Collections.min(queue.values());
+		Iterator iterator = queue.entrySet().iterator();
+		String route = "";
+		int dis = -1;
+		int[] now = { -1, -1 };
 
-		queue.put(step, "[" + now[0] + "," + now[1] + "]");
-		if (now[0] == dest[0] && now[1] == dest[1]) {
-			Iterator i = queue.entrySet().iterator();
-			while(i.hasNext())
-			{
-				Map.Entry en = (Entry) i.next();
-				System.out.println(en.getKey()+" "+en.getValue());
+		while (iterator.hasNext()) {
+			Map.Entry entry = (Entry) iterator.next();
+			if (entry.getValue().equals(min)) {
+				route = entry.getKey().toString();
+				dis = min;
+				int left = route.lastIndexOf("[");
+				int mid = route.lastIndexOf(",");
+				int end = route.lastIndexOf("]");
+				now[0] = Integer.parseInt(route.substring(left + 1, mid));
+				now[1] = Integer.parseInt(route.substring(mid+1, end));
+				queue.remove(entry.getKey());
+				break;
 			}
 		}
 		
+		if (now[0] == dest[0] && now[1] == dest[1]) {
+			System.out.print(route+"\n"+dis);
+			return;
+		}
 
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		map.put("N" + (now[0] - 1) + "," + now[1],
@@ -67,40 +84,44 @@ public class Aalgorithm {
 						- now[1] + 1, 2)));
 
 		if (now[0] - 1 < 0 || maze[now[0] - 1][now[1]] == 1
-				|| queue.containsValue("[" + (now[0] - 1) + "," + now[1] + "]")) {
+				|| route.contains("[" + (now[0] - 1) + "," + now[1] + "]")) {
 			map.remove("N" + (now[0] - 1) + "," + now[1]);
 		}
 		if (now[1] + 1 >= maze[0].length
 				|| maze[now[0]][now[1] + 1] == 1
-				|| queue.containsValue("[" + (now[0]) + "," + (now[1] + 1)
+				|| route.contains("[" + (now[0]) + "," + (now[1] + 1)
 						+ "]")) {
 			map.remove("E" + now[0] + "," + (now[1] + 1));
 		}
 		if (now[0] + 1 >= maze.length || maze[now[0] + 1][now[1]] == 1
-				|| queue.containsValue("[" + (now[0] + 1) + "," + now[1] + "]")) {
+				|| route.contains("[" + (now[0] + 1) + "," + now[1] + "]")) {
 			map.remove("S" + (now[0] + 1) + "," + now[1]);
 		}
 		if (now[1] - 1 < 0
 				|| maze[now[0]][now[1] - 1] == 1
-				|| queue.containsValue("[" + (now[0]) + "," + (now[1] - 1)
+				|| route.contains("[" + (now[0]) + "," + (now[1] - 1)
 						+ "]")) {
 			map.remove("W" + now[0] + "," + (now[1] - 1));
 		}
 
 		if (!map.isEmpty()) {
-			int min = Collections.min(map.values());
+			int min1 = Collections.min(map.values());
 			Iterator it = map.entrySet().iterator();
 			while (it.hasNext()) {
 				Map.Entry en = (Entry) it.next();
-				if (en.getValue().equals(min)) {
+				if (en.getValue().equals(min1)) {
 					String s = en.getKey().toString();
 					s = s.substring(1);
 					int coma = s.indexOf(",");
 					int[] next = { Integer.parseInt(s.substring(0, coma)),
 							Integer.parseInt(s.substring(coma + 1)) };
-					astar(next, queue, dest, step + 1);
+					s = route + "[" + next[0] + "," + next[1] + "]\n";
+					queue.put(s, dis + 1);
 				}
 			}
 		}
+		myAstar(queue, dest);
+
 	}
+
 }
